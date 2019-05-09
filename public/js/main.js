ed61d8d79e7a6827e74dcdca59ea2f85d8279bab
haduki1208 <syo4kante2@gmail.com>
2019-05-09 17:20:48 +0900

    pwa書いてみた

---
+++ public/js/main.js
@@ -0,0 +1,60 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/js/sw.js');
+}
+
+const clicklocalImageImportButton = () => {
+  document.querySelector('#localImageImport').click();
+};
+document
+  .querySelector('#localImageImportButton')
+  .addEventListener('click', clicklocalImageImportButton);
+
+const importLocalImage = ({ target }) => {
+  const file = target.files.item(0);
+  target.value = '';
+  document.querySelector('#localImage').src = URL.createObjectURL(file);
+};
+document
+  .querySelector('#localImageImport')
+  .addEventListener('change', importLocalImage);
+
+const onloadLocalImage = ({ target }) => {
+  const { width, height } = target;
+  const { canvas, ctx } = writeCanvas(target, width, height);
+  editCanvas(ctx, width, height);
+  exportImage(canvas);
+};
+document
+  .querySelector('#localImage')
+  .addEventListener('load', onloadLocalImage);
+
+const writeCanvas = (imgElement, height = 150, width = 300) => {
+  const canvas = document.querySelector('#editCanvas');
+  canvas.height = height;
+  canvas.width = width;
+  const ctx = canvas.getContext('2d');
+  ctx.drawImage(imgElement, 0, 0);
+  return { canvas, ctx };
+};
+
+const editCanvas = (ctx, width, height) => {
+  const imageData = ctx.getImageData(0, 0, width, height);
+  const newImageData = convertTransparentColor2White(imageData);
+  ctx.putImageData(newImageData, 0, 0);
+};
+
+const convertTransparentColor2White = imageData => {
+  for (let i = 3; i < imageData.data.length; i += 4) {
+    if (imageData.data[i] === 0) {
+      imageData.data[i] = 255;
+      imageData.data[i - 3] = 255;
+      imageData.data[i - 2] = 255;
+      imageData.data[i - 1] = 255;
+    }
+  }
+  return imageData;
+};
+
+const exportImage = canvas => {
+  document.querySelector('#editedImage').src = canvas.toDataURL();
+};
